411cc467a8879e421f1e002c719d7335e7a7abd9
Andy Harman <andyh.at.pendragon@gmail.com>
2014-03-22 10:52:02 +0000

    Refactored common CSS and JS out of example HTML files

---
+++ examples/assets/utils.js
@@ -0,0 +1,143 @@
+﻿/*!
+* Tony.JS WebAudio Library - http://github.com/pendragon-andyh/tony.js/
+* Copyright 2014, Andy Harman
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation
+* files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy,
+* modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the
+* Software is furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+* WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+* COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+* ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+;
+
+//Look for example code - and allow it to be run.
+function makeExampleCodeRunable(){
+	//Make example code runnable.
+	$("pre.example-code").hide().each(function() {
+		var $x=$(this);
+
+		//Get the JavaScript example and strip leading spaces.
+		var content=stripLeadingSpaces(this.innerText);
+		this.innerText=content;
+
+		//Add links before the example ("Show/Hide" and "Run example").
+		var isVisible=false;
+		$('<span> | <a class="example-toggle" href="javascript:">Show-example</a></span>').click(function() {
+			isVisible=!isVisible;
+			if(isVisible) {
+				$x.slideDown();
+				$(this).find("a").text("Hide-example");
+			} else {
+				$x.slideUp();
+				$(this).find("a").text("Show-example");
+			}
+		}).insertBefore(this);
+
+		$('<span> | <a class="example-run" href="javascript:">Run-example</a> |</span>').click(function() {
+			eval(content);
+		}).insertBefore(this);
+	});
+}
+
+//Convert content (from a PRE tag) so that tabs are converted to spaces - and the leading-space is removed.
+function stripLeadingSpaces(content) {
+	var newContent="", isFirstLine=true, isNonSpaceFound=false, leadingSpaceCount=0, currentLineSpaceCount=0;
+	var inputChars=content.replace(/\t/g, "  ").split("");
+	for(var i in inputChars) {
+		var c=inputChars[i];
+
+		if(!isNonSpaceFound&&c==" ") {
+			if(isFirstLine) {
+				leadingSpaceCount++;
+				currentLineSpaceCount++;
+			}
+
+			if(currentLineSpaceCount) {
+				c="";
+				currentLineSpaceCount--;
+			}
+		} else {
+			isNonSpaceFound=true;
+		}
+
+		if(c=="\n"||c=="\r") {
+			currentLineSpaceCount=leadingSpaceCount;
+			isFirstLine=false;
+			isNonSpaceFound=false;
+		}
+
+		newContent+=c;
+	}
+	return newContent;
+}
+
+//Wire-up the controls for a synth's settings panel.
+function setupSynthControls($parent, playFn, isAutoPlay) {
+	var player;
+
+	function autoPlay() {
+		if(isAutoPlay) {
+			//Apply some debouncing so that we don't get too many notes playing when range-slider is moving.
+			clearTimeout(player);
+			player=setTimeout(playFn, 200);
+		}
+	}
+
+	//Automatically play the sound when select-fields are changed.
+	$parent.find("select").change(function() {
+		autoPlay();
+	});
+
+	//Processing for range inputs.
+	$parent.find("input[type=range]").each(function() {
+	var $x=$(this);
+		
+		//Get the min/max values for the range.
+		var minValue=$x.attr("min")-0;
+		var maxValue=$x.attr("max")-0;
+		
+		//Range inputs only allow integers - so find out the factor that we are multiplying-by.
+		var multiplier=$x.attr("data-multiplier");
+		if(multiplier) {
+			multiplier-=0;
+		} else {
+			multiplier=1;
+		}
+
+		//Create a normal input field
+		var lastValue=$x.val()/multiplier;
+		var $tb=$('<input type="text" class="form-control"></input>').insertAfter($x).val(lastValue);
+
+		//When the range-slider changes then play the sound.
+		$x.change(function() {
+			$tb.val(lastValue=$x.val()/multiplier);
+			autoPlay();
+		});
+
+		//When the input-field changes then change the range-slider.
+		$tb.change(function() {
+			var val=$tb.val()*multiplier;
+			if(isNaN(val)) { val=lastValue; }
+			if(val<minValue) { val=minValue; }
+			if(val>maxValue) { val=maxValue; }
+			$x.val(val);
+			$x.change();
+		});
+	});
+}
+
+//Standardise the requestAnimationFrame.
+window.requestAnimFrame=(function() {
+	return window.requestAnimationFrame||
+		window.webkitRequestAnimationFrame||
+		window.mozRequestAnimationFrame||
+		window.oRequestAnimationFrame||
+		window.msRequestAnimationFrame||
+		function(callback, element) { window.setTimeout(callback, 1000/60); };
+})();
